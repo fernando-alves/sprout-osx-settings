343ee0d2c8941d8bda03540bba526eff10e342b3
Fernando <fernandoasfj@gmail.com>
2016-07-03 20:06:31 -0300

    Allow user to enable/disable tap to click

--- recipes/trackpad.rb
+++ recipes/trackpad.rb
@@ -1,17 +1,16 @@
 osx_defaults 'turn on tap to click' do
   domain 'com.apple.AppleMultitouchTrackpad'
   key 'Clicking'
-  boolean boolean node['trackpad']['tap_to_click']
+  boolean node['trackpad']['tap_to_click']
 end
 
-# osx_defaults 'turn on three finger drag' do
-#   domain 'com.apple.AppleMultitouchTrackpad'
-#   key 'TrackpadThreeFingerDrag'
-#   boolean boolean node['trackpad']['three_finger_drag']
-# end
+osx_defaults 'set tap behaviour globaly' do
+  domain 'NSGlobalDomain'
+  key 'com.apple.mouse.tapBehavior'
+  integer node['trackpad']['tap_to_click'] ? 1 : 0
+end
 
-osx_defaults 'turn on tap to click' do
-  domain 'com.apple.driver.AppleBluetoothMultitouch.trackpad'
-  key 'Clicking'
-  boolean boolean node['trackpad']['tap_to_click']
+execute 'set tap behaviour for current user' do
+  user node['sprout']['user']
+  command "defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int #{node['trackpad']['tap_to_click'] ? 1 : 0}"
 end
