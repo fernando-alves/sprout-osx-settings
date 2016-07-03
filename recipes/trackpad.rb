5a172ba563ecfdcef808a8e7d8ebde167978c7ee
Fernando <fernandoasfj@gmail.com>
2016-07-03 15:44:44 -0300

    Change on bluetooh trackpad as well

--- recipes/trackpad.rb
+++ recipes/trackpad.rb
@@ -4,8 +4,14 @@ osx_defaults 'turn on tap to click' do
   boolean boolean node['trackpad']['tap_to_click']
 end
 
-osx_defaults 'turn on three finger drag' do
-  domain 'com.apple.AppleMultitouchTrackpad'
-  key 'TrackpadThreeFingerDrag'
-  boolean boolean node['trackpad']['three_finger_drag']
+# osx_defaults 'turn on three finger drag' do
+#   domain 'com.apple.AppleMultitouchTrackpad'
+#   key 'TrackpadThreeFingerDrag'
+#   boolean boolean node['trackpad']['three_finger_drag']
+# end
+
+osx_defaults 'turn on tap to click' do
+  domain 'com.apple.driver.AppleBluetoothMultitouch.trackpad'
+  key 'Clicking'
+  boolean boolean node['trackpad']['tap_to_click']
 end
